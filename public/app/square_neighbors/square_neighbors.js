b592e66ed37dd9d68252ece7b80759df742187ef
Matias Risco <matiasriscolagos@gmail.com>
2023-10-15 10:04:55 +0200

    Fix logic bugs in square_neighboors

--- public/app/square_neighbors/square_neighbors.js
+++ public/app/square_neighbors/square_neighbors.js
@@ -15,30 +15,45 @@ class SquareNeighbors {
     const { positionX } = this;
     const { positionY } = this;
     const left = positionX - 1 < 0 ? sizeX - 1 : positionX - 1;
-    const rigth = positionX + 1 > sizeX ? 0 : positionX + 1;
+    const rigth = positionX + 1 >= sizeX ? 0 : positionX + 1;
     const up = positionY - 1 < 0 ? sizeY - 1 : positionY - 1;
-    const down = positionY + 1 > sizeY ? 0 : positionY + 1;
+    const down = positionY + 1 >= sizeY ? 0 : positionY + 1;
 
     const row = [left, positionX, rigth];
     const colum = [up, positionY, down];
 
-    for (let locationX = 0; locationX < row.length; locationX++)
-      for (let locationY = 0; locationY < colum.length; locationY++) {
-        if (!cells[row[locationY]]) cells[row[locationY]] = {};
+    for (let locationY = 0; locationY < colum.length; locationY++)
+      for (let locationX = 0; locationX < row.length; locationX++) {
+        if (cells[colum[locationY]] === undefined) cells[colum[locationY]] = {};
 
-        if (cells[row[locationY]][colum[locationX]] === undefined)
-          cells[row[locationY]][colum[locationX]] = new SquareNeighbors(
-            row[locationY],
-            colum[locationX],
+        if (cells[colum[locationY]][row[locationX]] === undefined)
+          cells[colum[locationY]][row[locationX]] = new SquareNeighbors(
+            row[locationX],
+            colum[locationY],
             false,
             1,
           );
         else if (
           this.alive &&
-          (positionX !== row[locationY] || positionY !== colum[locationX])
+          !(positionX === row[locationX] && positionY === colum[locationY])
         )
-          cells[row[locationY]][colum[locationX]].neighbors++;
+          cells[colum[locationY]][row[locationX]].addNeighbors();
       }
+  }
+
+  isAlive() {
+    let { alive } = this;
+    const { neighbors } = this;
+    alive = (alive && neighbors === 2) || neighbors === 3;
+    this.alive = alive;
+  }
+
+  resetNeighbors() {
+    this.neighbors = 0;
+  }
+
+  addNeighbors() {
+    this.neighbors++;
   }
 }
 
